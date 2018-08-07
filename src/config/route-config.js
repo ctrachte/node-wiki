2ecbf64ec6b1ad4d504c9e950327200876f45b72
Caleb Trachte <cetrachte@hotmail.com>
2018-08-07 11:40:32 -0500

    add users specs, route, controller, db migration files, model, and partial views

--- src/config/route-config.js
+++ src/config/route-config.js
@@ -2,6 +2,7 @@ module.exports = {
   init(app){
     const mockAuth = require("../../spec/support/mock-auth.js");
     const staticRoutes = require("../routes/static");
+    const userRoutes = require("../routes/users");
 
 
     if(process.env.NODE_ENV == "test") {
@@ -9,6 +10,6 @@ module.exports = {
     };
 
     app.use(staticRoutes);
-
+    app.use(userRoutes);
   }
 }
---
+++ src/db/migrations/20180807152523-create-user.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING,
+        unique: true,
+        validate: {
+          isEmail: { msg: "must be a valid email" }
+        },
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        defaultValue: "member"
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
